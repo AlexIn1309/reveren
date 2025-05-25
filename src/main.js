7076895de80a1b335da12b16c340afc131a6e233
AlexIn1309 <moran.graciano.cesar@gmail.com>
2025-05-25 16:13:29 -0600

    Integracion de componenetes, navbar, presentacion, catalog v2

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+// Presentacion de imagenes de presentacion
+document.addEventListener("DOMContentLoaded", function () {
+  const img1 = document.getElementById("img1");
+  const img2 = document.getElementById("img2");
+  setTimeout(() => {
+    img2.classList.remove("oculto");
+    setTimeout(() => {
+      img2.classList.add("oculto");
+    }, 2000);
+  }, 3000);
+});
