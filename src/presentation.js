2d5c153efbdff53ef7220dfe0ad00461d7ddf57d
AlexIn1309 <moran.graciano.cesar@gmail.com>
2025-05-25 15:37:58 -0600

    navbar y presentacion v1

---
+++ src/presentation.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const img1 = document.getElementById("img1");
+  const img2 = document.getElementById("img2");
+  setTimeout(() => {
+    img2.classList.remove("oculto");
+    setTimeout(() => {
+      img2.classList.add("oculto");
+    }, 2000);
+  }, 3000);
+});
