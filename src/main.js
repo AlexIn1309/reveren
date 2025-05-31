2b523ed7c7c681c8f84ea8445c5e4767d277075e
AlexIn1309 <moran.graciano.cesar@gmail.com>
2025-05-31 00:34:55 -0600

    vista perfil y presentacion

--- src/main.js
+++ src/main.js
@@ -9,3 +9,59 @@ document.addEventListener("DOMContentLoaded", function () {
     }, 2000);
   }, 3000);
 });
+
+  const userIcon = document.getElementById('userIcon');
+  const userDropdown = document.getElementById('userDropdown');
+  userIcon.addEventListener('click', () => {
+    userDropdown.style.display = userDropdown.style.display === 'block' ? 'none' : 'block';
+  });
+
+  function showLoginModal() {
+    closeModal('registerModal');
+    document.getElementById('loginModal').style.display = 'flex';
+  }
+
+  function showRegisterModal() {
+    closeModal('loginModal');
+    document.getElementById('registerModal').style.display = 'flex';
+  }
+
+  function closeModal(id) {
+    document.getElementById(id).style.display = 'none';
+  }
+
+  function switchToRegister() {
+    closeModal('loginModal');
+    showRegisterModal();
+  }
+
+  function switchToLogin() {
+    closeModal('registerModal');
+    showLoginModal();
+  }
+
+  function togglePassword(id) {
+    const field = document.getElementById(id);
+    field.type = field.type === 'password' ? 'text' : 'password';
+  }
+
+    function loginUser() {
+    closeModal('loginModal');
+
+    document.getElementById('userMenuLoggedOut').style.display = 'none';
+    document.getElementById('userMenuLoggedIn').style.display = 'flex';
+
+    document.getElementById('img1').classList.add('oculto');
+    document.getElementById('img2').classList.remove('oculto');
+  }
+
+  function logoutUser() {
+    document.getElementById('userMenuLoggedOut').style.display = 'flex';
+    document.getElementById('userMenuLoggedIn').style.display = 'none';
+
+    document.getElementById('img1').classList.remove('oculto');
+    document.getElementById('img2').classList.add('oculto');
+  }
+  function closeModal(id) {
+    document.getElementById(id).style.display = 'none';
+  }
